d7c7f3e0c26d2246fa7e1429c6aee8251bacdf41
Tasim <118922118+TasimDev@users.noreply.github.com>
2023-09-13 09:41:04 +0300

    тасимцхоо

---
+++ script.js
@@ -0,0 +1,22 @@
+const text = document.querySelector('.text-rotate p');
+
+text.innerHTML = text.innerText.split("").map((char, i) =>
+    `<span style = "transform:rotate(${i * 17.1}deg)">${char}</span>`
+).join(" ");
+
+window.addEventListener('load', () => {
+    gsap.to('.loader', {
+        y: '100%',
+        duration: 1,
+        ease: 'sine',
+        delay: 2
+    })
+    gsap.to('.loader .title', {
+        scale: 1,
+        opacity: 1,
+        duration: 2,
+        ease: 'power1',
+    })
+})
+
+
